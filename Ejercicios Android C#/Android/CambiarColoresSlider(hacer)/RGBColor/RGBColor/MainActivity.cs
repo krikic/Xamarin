1cf3b1bb6730f55326ccf9618740d7342e54dfd9
krikic <david.curso.oracle@gmail.com>
2019-02-24 10:50:56 +0100

    Adding a RGB Color in Xamarin Android
    The project not work yet

---
+++ Ejercicios Android C#/Android/CambiarColoresSlider(hacer)/RGBColor/RGBColor/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Graphics;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Webkit;
+
+namespace RGBColor
+{
+    [Activity(Label = "RGBColor", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        SeekBar skRed;
+        SeekBar skGreen;
+        SeekBar skBlue;
+
+     
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+
+            skRed = FindViewById<SeekBar>(Resource.Id.redBar);
+            skGreen = FindViewById<SeekBar>(Resource.Id.greenBar);
+            skBlue = FindViewById<SeekBar>(Resource.Id.blueBar);
+
+            WebView myView = FindViewById<WebView>(Resource.Id.customView);
+
+            Button colorChange = FindViewById<Button>(Resource.Id.myButton);
+
+
+
+            colorChange.setOnClickListener(new View.OnClickListener() {
+         
+            public void onClick(View v)
+            {
+
+
+
+
+                myView.setDrawingCacheEnabled(true);
+
+                myView.buildDrawingCache();
+
+
+                // bm is your required bitmap
+                Bitmap bm = myView.getDrawingCache();
+
+                Toast.makeText(MainActivity.this, "Bitmap Ready", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        skRed.setOnSeekBarChangeListener(changeListener);
+        skGreen.setOnSeekBarChangeListener(changeListener);
+        skBlue.setOnSeekBarChangeListener(changeListener);
+    }
+
+    SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+        
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+    {
+        myView.setColor(skRed.getProgress(), skGreen.getProgress(), skBlue.getProgress());
+    }
+
+  
+        public void onStartTrackingTouch(SeekBar seekBar)
+    {
+
+    }
+
+ 
+        public void onStopTrackingTouch(SeekBar seekBar)
+    {
+
+    }
+};
+
+
+        }
+    }
+}
+
---
+++ Ejercicios Android C#/Android/CambiarColoresSlider(hacer)/RGBColor/RGBColor/RBGView.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace RGBColor
+{
+    [Activity(Label = "RBGView")]
+    public class RBGView : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        public class RgbView extends View
+        {
+
+            Paint p=new Paint();
+        public RgbView(Context context)
+        {
+            super(context);
+            init(null, 0);
+        }
+
+        public RgbView(Context context, AttributeSet attrs)
+        {
+            super(context, attrs);
+            init(attrs, 0);
+        }
+
+        public RgbView(Context context, AttributeSet attrs, int defStyle)
+        {
+            super(context, attrs, defStyle);
+            init(attrs, defStyle);
+        }
+
+        private void init(AttributeSet attrs, int defStyle)
+        {
+            p.setColor(Color.RED);
+        }
+
+        public void setColor(int Red, int Green, int Blue)
+        {
+
+            p.setARGB(255, Red, Green, Blue);
+            invalidate();
+
+        }
+
+        @Override
+    protected void onDraw(Canvas canvas)
+        {
+            super.onDraw(canvas);
+
+            canvas.drawRect(0, 0, getHeight(), getWidth(), p);
+        }
+
+
+
+
+    }
+}
