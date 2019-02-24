ffd9000dd3ada2aae7c9348d1fbfeae4d13be91c
krikic <david.curso.oracle@gmail.com>
2019-02-24 22:25:21 +0100

    Create Animated RBG Colors IOS
    Implement exercise about changing color depending of choose button

---
+++ Ejercicios IOS C#/IOS/AnimatedRGBColors/AnimatedRGBColors/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using UIKit;
+
+namespace AnimatedRGBColors
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle) { }
+       
+
+
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        partial void BtnRed_TouchUpInside(UIButton sender)
+        {
+            View.BackgroundColor = UIColor.Red;
+        }
+
+        partial void BtnGreen_TouchUpInside(UIButton sender)
+        {
+            View.BackgroundColor = UIColor.Green;
+        }
+
+        partial void BtnBlue_TouchUpInside(UIButton sender)
+        {
+            View.BackgroundColor = UIColor.Blue;
+        }
+
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+
+
+       
+    }
+}
---
+++ Ejercicios IOS C#/IOS/AnimatedRGBColors/AnimatedRGBColors/ViewController.designer.cs
@@ -0,0 +1,58 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace AnimatedRGBColors
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_Green { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtnBlue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtnRed { get; set; }
+
+        [Action ("BtnBlue_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnBlue_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("BtnGreen_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnGreen_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("BtnRed_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnRed_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Btn_Green != null) {
+                Btn_Green.Dispose ();
+                Btn_Green = null;
+            }
+
+            if (BtnBlue != null) {
+                BtnBlue.Dispose ();
+                BtnBlue = null;
+            }
+
+            if (BtnRed != null) {
+                BtnRed.Dispose ();
+                BtnRed = null;
+            }
+        }
+    }
+}
